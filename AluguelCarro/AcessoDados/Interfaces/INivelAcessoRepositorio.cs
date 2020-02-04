69145c60c45bca99bed4d3452b59310d782db72e
Jackson almeida de oliveira <jacksonalmeida6@gmail.com>
2020-02-04 13:36:01 -0200

    Projeto Final

---
+++ AluguelCarro/AcessoDados/Interfaces/INivelAcessoRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using AluguelCarro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AluguelCarro.AcessoDados.Interfaces
+{
+    public interface INivelAcessoRepositorio : IRepositorioGenerico<NiveisAcesso>
+    {
+        Task<bool> NivelAcessoExiste(string nivelAcesso);
+        
+        new Task Atualizar(NiveisAcesso niveisAcesso);
+    }
+}
